31a26de04e79f59802423326960776d664c27e5c
mylab12345 <mekdb30@gmail.com>
2025-08-25 19:43:16 +0530

    added bug

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ import spark.servlet.SparkApplication;
 
 import static spark.Spark.get;
 
-public class HelloWorld implements SparkApplication {
+#public class HelloWorld implements SparkApplication {
 	public static void main(String[] args) {
 		new HelloWorld().init();
 	}
